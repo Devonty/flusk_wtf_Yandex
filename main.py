8980cb8737ca043c6a030fd1ffb5f62a5f39c571
Devonty <Devonty@yandex.ru>
2022-02-15 16:46:29 +0300

    task_1

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, url_for, request, render_template
+import requests
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+from LoginForm import LoginForm
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'что я тут напишу - это моё дело'
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('task_1.html', title=title)
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
