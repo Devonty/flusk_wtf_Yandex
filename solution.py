155e83e8abc6f1417eeb4f58fe8a69a5662e9b80
Devonty <Devonty@yandex.ru>
2022-02-25 16:46:58 +0300

    task_3

--- solution.py
+++ solution.py
@@ -1,6 +1,4 @@
-from flask import Flask, url_for, request, render_template
-import requests
-from flask_wtf import FlaskForm
+from flask import Flask, render_template
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 from LoginForm import LoginForm
@@ -9,14 +7,21 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'что я тут напишу - это моё дело'
 
-@app.route('/<title>')
-@app.route('/index/<title>')
-def index(title):
+@app.route('/')
+@app.route('/index')
+def index(title='name'):
     return render_template('task_1.html', title=title)
 
 @app.route('/training/<prof>')
 def training(prof):
     return render_template('task_2.html',prof=prof, title='На марс')
+
+
+@app.route('/list_prof/<param>')
+def list_prof(param):
+    list_ = [f'Профессия{i}' for i in range(16)]
+    return render_template('task_3.html',param=param, title='На марс', list_=list_)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
